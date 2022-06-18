ae927825eb21ceedaafc3dad7078983e637a630c
Evgeniy Kozlov <41782453+xyz27900@users.noreply.github.com>
2022-06-18 13:11:25 +0400

    Work on Section 1 (#6)
    * Work on Section 1
    
    * Move mixins
    
    * Remove HelloWorld.vue
    
    * Use v-for
    
    * Rename folder
    
    * Sync with main
    
    Co-authored-by: Manu Ustenko <ustenko.mi@gmail.com>

--- course/section1/frontend/src/router/index.js
+++ course/section1/frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
+      component: import("@/views/HomeView.vue"),
     },
   ],
 });
