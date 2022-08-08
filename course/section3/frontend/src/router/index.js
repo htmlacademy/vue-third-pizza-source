1509f9837aa6e133b54f418b7e7132d3ef468ccd
Evgeniy Kozlov <kozlov.work.52@gmail.com>
2022-08-09 03:40:49 +0400

    Import routes from file

--- course/section3/frontend/src/router/index.js
+++ course/section3/frontend/src/router/index.js
@@ -1,49 +1,5 @@
-import { createRouter, createWebHistory, RouterView } from "vue-router";
-
-const routes = [
-  {
-    path: "/",
-    component: RouterView,
-    children: [
-      {
-        path: "",
-        name: "home",
-        component: () => import("@/views/HomeView.vue"),
-        meta: { layout: "DefaultLayout" },
-      },
-      {
-        path: "login",
-        name: "login",
-        component: () => import("@/views/LoginView.vue"),
-        meta: { layout: "SimpleLayout" },
-      },
-    ],
-  },
-  {
-    path: "/cart",
-    name: "cart",
-    component: () => import("@/views/CartView.vue"),
-    meta: { layout: "DefaultLayout" },
-  },
-  {
-    path: "/user",
-    name: "user",
-    component: () => import("@/views/UserView.vue"),
-    meta: { layout: "DefaultLayout" },
-    children: [
-      {
-        path: "orders",
-        name: "orders",
-        component: () => import("@/views/OrdersView.vue"),
-      },
-      {
-        path: "profile",
-        name: "profile",
-        component: () => import("@/views/ProfileView.vue"),
-      },
-    ],
-  },
-];
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "./routes";
 
 export const router = createRouter({
   routes,
