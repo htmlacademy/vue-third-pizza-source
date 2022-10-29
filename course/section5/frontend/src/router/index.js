5b8e796c9fc25681bb4b3401e775e95429d42146
Evgeniy Kozlov <41782453+xyz27900@users.noreply.github.com>
2022-10-30 03:25:37 +0400

    Section 5 pizza (#18)

--- course/section5/frontend/src/router/index.js
+++ course/section5/frontend/src/router/index.js
@@ -1,20 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { routes } from "./routes";
+import { middlewarePipeline } from "@/middlewares/middlewarePipeline";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
-    },
-  ],
+export const router = createRouter({
+  routes,
+  history: createWebHistory(),
 });
 
-export default router;
+middlewarePipeline(router);
