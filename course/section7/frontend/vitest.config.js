38077823398c14c54113c99f5a9abb7fdc99fa0b
Evgeniy Kozlov <kozlov.work.52@gmail.com>
2022-11-19 22:07:40 +0500

    Remove happy dom

--- course/section7/frontend/vitest.config.js
+++ course/section7/frontend/vitest.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   test: {
-    environment: "happy-dom",
+    environment: "jsdom",
   },
   resolve: {
     alias: {
