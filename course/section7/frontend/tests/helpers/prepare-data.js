0d67047e088631f4a4855eaaf7927cec282c3596
Evgeniy Kozlov <kozlov.work.52@gmail.com>
2022-11-20 16:11:18 +0500

    Fix tests

--- course/section7/frontend/tests/helpers/prepare-data.js
+++ course/section7/frontend/tests/helpers/prepare-data.js
@@ -3,11 +3,13 @@ import doughJSON from "../__mocks__/data/dough.json";
 import ingredientsJSON from "../__mocks__/data/ingredients.json";
 import saucesJSON from "../__mocks__/data/sauces.json";
 import sizesJSON from "../__mocks__/data/sizes.json";
+import miscJSON from "../__mocks__/data/misc.json";
 
 export const prepareData = () => {
   const dataStore = useDataStore();
   dataStore.doughs = doughJSON;
   dataStore.ingredients = ingredientsJSON;
+  dataStore.misc = miscJSON;
   dataStore.sauces = saucesJSON;
   dataStore.sizes = sizesJSON;
   return dataStore;
